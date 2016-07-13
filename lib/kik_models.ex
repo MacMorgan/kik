a890720aa438ef3aaefed2c414c94f0816cbc657
Mac Morgan <mac@macmorgan.com>
2016-07-13 10:58:56 -0400

    update kik

--- lib/kik_models.ex
+++ lib/kik_models.ex
@@ -17,7 +17,7 @@ defmodule Kik.Models.Config do
   defstruct [:webhook, :features]
 
   def parse(json) do
-    Poison.decode!(json, as: %Kik.Models.Config{})
+    Poison.decode!(json) #, as: %Kik.Models.Config{}
   end
 
   @type t :: %Kik.Models.Config{
