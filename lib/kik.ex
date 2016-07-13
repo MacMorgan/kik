be6eb217440d9abd95048fc30f74bae63f075fdd
Mac Morgan <mac@macmorgan.com>
2016-07-13 10:40:34 -0400

    update kik

--- lib/kik.ex
+++ lib/kik.ex
@@ -1,24 +1,19 @@
 defmodule Kik do
   require Logger
   require Kik.RequestManager
+  require HTTPotion
 
   def config do
     Kik.Models.Config.parse(manager.get("config").body)
   end
 
   def config(webhook) do
-    Kik.RequestManager.post(
-      "config",
+    HTTPotion.post(
+      "https://api.kik.com/v1/config",
       [
-        body: %{
-          "webhook": "https://0ef0244d.ngrok.io/kik/webhook",
-          "features": %{
-            "manuallySendReadReceipts": false,
-            "receiveReadReceipts": false,
-            "receiveDeliveryReceipts": false,
-            "receiveIsTyping": false
-          }
-        }
+        basic_auth: {"tonicdesignbot", "ecdaa93e-f233-4e8c-8c74-7caaee896e1d"},
+        body: "{\"webhook\": \"https://0ef0244d.ngrok.io/kik/webhook\",\"features\": {\"manuallySendReadReceipts\": false,\"receiveReadReceipts\": false,\"receiveDeliveryReceipts\": false,\"receiveIsTyping\": false}}",
+        headers: ["Content-Type": "application/json"]
       ]
     )
   end
